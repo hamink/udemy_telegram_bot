5d5dd4d60672ddc0508000018bd13539f4c9f9fb
--unset <hamin@atomrigs.io>
2021-12-28 14:54:27 -0700

    test 12

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ def check_reminders():
         time.sleep(INTERVAL)
 
 def generate_handler(update: Update, context: CallbackContext):
+    print(context.args[0])
     url = create_deep_linked_url(update.message.chat.username, update.message.forward_from, group=True)
     update.message.reply_text(text="Share it with your friends: %s.\n Copy the link and share it with them" % url)
 

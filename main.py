fa177c1fa7ba64a7d0462fd0c9b65594ff9d87a6
--unset <hamin@atomrigs.io>
2021-12-28 17:23:10 -0700

    test 17

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from telegram.ext import (Updater, CommandHandler, ConversationHandler, MessageHandler,
                           Filters, CallbackContext)
-from telegram import KeyboardButton, ReplyKeyboardMarkup, Update, MessageEntity
+from telegram import KeyboardButton, ReplyKeyboardMarkup, Update, ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.utils.helpers import create_deep_linked_url
+from telegram.utils import helpers
 from data_source import DataSource
 import os
 import threading
@@ -33,8 +34,61 @@ else:
     logger.error("No mode specified!")
     sys.exit(1)
 
+    # Define constants that will allow us to reuse the deep-linking parameters.
+    CHECK_THIS_OUT = "check-this-out"
+    USING_ENTITIES = "using-entities-here"
+    SO_COOL = "so-cool"
+
+def start(update, context):
+    """Send a deep-linked URL when the command /start is issued."""
+    bot = context.bot
+    url = helpers.create_deep_linked_url(
+        bot.get_me().username, CHECK_THIS_OUT, group=True
+    )
+    text = "Feel free to tell your friends about it:\n\n" + url
+    update.message.reply_text(text)
+
+
+def deep_linked_level_1(update, context):
+    """Reached through the CHECK_THIS_OUT payload"""
+    bot = context.bot
+    url = helpers.create_deep_linked_url(bot.get_me().username, SO_COOL)
+    text = (
+        "Awesome, you just accessed hidden functionality! "
+        " Now let's get back to the private chat."
+    )
+    keyboard = InlineKeyboardMarkup.from_button(
+        InlineKeyboardButton(text="Continue here!", url=url)
+    )
+    update.message.reply_text(text, reply_markup=keyboard)
+
+
+def deep_linked_level_2(update, context):
+    """Reached through the SO_COOL payload"""
+    bot = context.bot
+    url = helpers.create_deep_linked_url(bot.get_me().username, USING_ENTITIES)
+    text = (
+        "You can also mask the deep-linked URLs as links: "
+        "[▶️ CLICK HERE]({0}).".format(url)
+    )
+    update.message.reply_text(
+        text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
+    )
+
+
+def deep_linked_level_3(update, context):
+    """Reached through the USING_ENTITIES payload"""
+    payload = context.args
+    update.message.reply_text(
+        "Congratulations! This is as deep as it gets 👏🏻\n\n"
+        "The payload was: {0}".format(payload)
+    )
+
+
 
 def start_handler(update, context):
+    bot = context.bot
+
     param_value = context.args[0]
     update.message.reply_text('Value is ' + param_value)
     update.message.reply_text("Hello, master!", reply_markup=add_reminder_button())
@@ -87,6 +141,17 @@ if __name__ == '__main__':
     updater = Updater(TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", start_handler))
     updater.dispatcher.add_handler(CommandHandler("generate", generate_handler))
+    DEEPLINKING_HANDLERS = [
+    CommandHandler("deeplinking", deep_linked_level_1, Filters.regex(CHECK_THIS_OUT)),
+    CommandHandler("deeplinking", deep_linked_level_2, Filters.regex(SO_COOL)),
+    CommandHandler(
+        "deeplinking",
+        deep_linked_level_3,
+        Filters.regex(USING_ENTITIES),
+        pass_args=True,
+    ),
+    CommandHandler("deeplinking", start),
+]
     conv_handler = ConversationHandler(
         entry_points=[MessageHandler(Filters.regex(ADD_REMINDER_TEXT), add_reminder_handler)],
         states={
